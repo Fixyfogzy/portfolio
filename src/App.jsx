45000b92d0ad961e645f124222f8d7b97b214e08
Dally Mofiyinfoluwa <dallymofiyinfoluwa@gmail.com>
2026-06-14 13:58:22 +0100

    fix portfolio import case

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
-import Portfolio from './pages/Portfolio'
+import Portfolio from './pages/portfolio'
 import About from './pages/About'
 import Contact from './pages/Contact'
 
